91e570869fb4450587e1b01c636a7bb3b32d56cc
Patryk Sroka <sunder.fresh@gmail.com>
2015-03-12 00:25:45 +0100

    commit 6

---
+++ comment.php
@@ -0,0 +1,18 @@
+<?php
+include("db.php");
+if(isset($_POST['submit']))
+{
+	$idfilmu = filter_var($_POST["idfilmu"], FILTER_SANITIZE_NUMBER_INT);
+	$name=$_POST['namename'];
+	$message=$_POST['message'];
+	$insert=mysql_query("insert into recenzja_$idfilmu(name,message)values('$name','$message')")or die(mysql_error());
+	
+	
+
+session_start();
+$_SESSION["root"] = $idfilmu;
+
+header('Location:opis.php');
+
+	}
+?>
