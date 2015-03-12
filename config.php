0fde665bf9b2cbf5558c18621b2f69744be10b1f
szamik <szamik21@interia.eu>
2015-03-12 10:45:34 +0100

    Update config.php
    Zmiana danych bazy

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $currency = 'PLN'; 
 
-$db_username = 'root';
-$db_password = '';
-$db_name = 'database_sklep';
-$db_host = 'localhost';
- mysql_connect("localhost", "root", "") or die(mysql_error()); 
+$db_username = 's176839';
+$db_password = 'DgPir2zw';
+$db_name = 's176839';
+$db_host = 'sbazy.uek.krakow.pl';
+ mysql_connect("sbazy.uek.krakow.pl", "s176839", "DgPir2zw") or die(mysql_error()); 
  mysql_select_db("database_sklep") or die(mysql_error()); 
  mysql_query("SET CHARSET utf8");
  mysql_query("SET NAMES 'utf8' COLLATE 'utf8_polish_ci'"); 
