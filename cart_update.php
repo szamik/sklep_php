654bfb1a53dcbdd25d403146afe0342ba08d4052
Patryk Sroka <sunder.fresh@gmail.com>
2015-03-12 10:18:42 +0100

    update8

--- cart_update.php
+++ cart_update.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-include_once("config.php");
-
+include("config.php");
 
 if(isset($_GET["emptycart"]) && $_GET["emptycart"]==1)
 {
@@ -12,52 +11,50 @@ if(isset($_GET["emptycart"]) && $_GET["emptycart"]==1)
 
 if(isset($_POST["type"]) && $_POST["type"]=='add')
 {
-	$product_code 	= filter_var($_POST["product_code"], FILTER_SANITIZE_STRING);
+	$product_code 	= filter_var($_POST["product_code"], FILTER_SANITIZE_STRING); 
 	$product_qty 	= filter_var($_POST["product_qty"], FILTER_SANITIZE_NUMBER_INT);
-	$return_url 	= base64_decode($_POST["return_url"]);
-	
-	
+	$return_url 	= base64_decode($_POST["return_url"]); 
+
 	$results = $mysqli->query("SELECT product_name,price FROM products WHERE product_code='$product_code' LIMIT 1");
 	$obj = $results->fetch_object();
 	
-	if ($results) { 
-		
+	if ($results) {
 		
 		$new_product = array(array('name'=>$obj->product_name, 'code'=>$product_code, 'qty'=>$product_qty, 'price'=>$obj->price));
 		
 		if(isset($_SESSION["products"])) 
 		{
-			$found = false; //set found item to false
+			$found = false; 
 			
-			foreach ($_SESSION["products"] as $cart_itm) 
+			foreach ($_SESSION["products"] as $cart_itm)
 			{
 				if($cart_itm["code"] == $product_code){ 
 
 					$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$product_qty, 'price'=>$cart_itm["price"]);
 					$found = true;
 				}else{
-				
+
 					$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$cart_itm["qty"], 'price'=>$cart_itm["price"]);
 				}
 			}
 			
-			if($found == false) //we didn't find item in array
+			if($found == false) 
 			{
-				
+
 				$_SESSION["products"] = array_merge($product, $new_product);
 			}else{
-				
+
 				$_SESSION["products"] = $product;
 			}
 			
 		}else{
-		
+
 			$_SESSION["products"] = $new_product;
 		}
 		
 	}
 	
-	
+
 	header('Location:'.$return_url);
 }
 
@@ -70,10 +67,9 @@ if(isset($_GET["removep"]) && isset($_GET["return_url"]) && isset($_SESSION["pro
 	
 	foreach ($_SESSION["products"] as $cart_itm) 
 	{
-		if($cart_itm["code"]!=$product_code){ 
+		if($cart_itm["code"]!=$product_code){
 			$product[] = array('name'=>$cart_itm["name"], 'code'=>$cart_itm["code"], 'qty'=>$cart_itm["qty"], 'price'=>$cart_itm["price"]);
 		}
-		
 		
 		$_SESSION["products"] = $product;
 	}
