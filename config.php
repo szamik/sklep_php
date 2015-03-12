37e7616af7ae9c9294f586019b78c3531a9e54df
Patryk Sroka <sunder.fresh@gmail.com>
2015-03-12 10:16:16 +0100

    up4

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 $currency = 'PLN'; 
 
-$db_username = 's176839';
-$db_password = 'DgPir2zw';
-$db_name = 's176839';
-$db_host = 'sbazy.uek.krakow.pl';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'database_sklep';
+$db_host = 'localhost';
+ mysql_connect("localhost", "root", "") or die(mysql_error()); 
+ mysql_select_db("database_sklep") or die(mysql_error()); 
+ mysql_query("SET CHARSET utf8");
+ mysql_query("SET NAMES 'utf8' COLLATE 'utf8_polish_ci'"); 
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
