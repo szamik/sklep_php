ead64c7b206551420f92120993b2c4c0644d75ad
Patryk Sroka <sunder.fresh@gmail.com>
2015-03-12 10:18:15 +0100

    up7

--- comment.php
+++ comment.php
@@ -1,13 +1,11 @@
 <?php
-include("db.php");
+include("config.php");
 if(isset($_POST['submit']))
 {
 	$idfilmu = filter_var($_POST["idfilmu"], FILTER_SANITIZE_NUMBER_INT);
 	$name=$_POST['namename'];
 	$message=$_POST['message'];
 	$insert=mysql_query("insert into recenzja_$idfilmu(name,message)values('$name','$message')")or die(mysql_error());
-	
-	
 
 session_start();
 $_SESSION["root"] = $idfilmu;
